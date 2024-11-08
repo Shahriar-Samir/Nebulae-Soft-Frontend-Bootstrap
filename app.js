5d71dd3318e05528d7e63cda9521266d3cfab9ba
Shahriar-Samir <shabusiness035@gmail.com>
2024-11-08 11:11:58 +0600

    add: all required featues

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 
+new Swiper('.swiper', {
+
+    loop: true,
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        1024:{
+            slidesPerView:2
+        },
+    }
+
+  });
 
--- readme.md
+++ readme.md
@@ -1,6 +1,4 @@
 # How to run on local machine
 - Download the zip or use git clone.
 - If you download the zip file, unzip it and go the Nebulae-Soft-Frontend-Bootstrap-main directory.
-- Open the integrated terminal of that directory.
-- Give the command npm install.
 - Run the app with live server.
